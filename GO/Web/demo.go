6e6d944b18da4539c8adb5f40a49c466b4d1c1e2
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2023-11-27 14:59:52 +0000

    Update demo.go

--- GO/Web/demo.go
+++ GO/Web/demo.go
@@ -1,25 +1,27 @@
 package main
 
 import (
-	"github.com/alexedwards/flow"
 	"log"
 	"net/http"
 	"text/template"
 )
 
 func main() {
-	mux := flow.New()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		renderHTML(w, "index.html")
-	}, "GET")
-	err := http.ListenAndServe(":8080", mux)
+	})
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-
 func renderHTML(w http.ResponseWriter, fileName string) {
-	err := template.Must(template.ParseFiles(fileName)).ExecuteTemplate(w, fileName, nil)
+	tmpl, err := template.ParseFiles(fileName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.ExecuteTemplate(w, fileName, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
