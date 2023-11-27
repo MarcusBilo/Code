6ac7969790643a2b3663a0855fe68c2ea25f823c
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2023-11-27 11:13:23 +0000

    Update demo.go

--- GO/Web/demo.go
+++ GO/Web/demo.go
@@ -1,9 +1,6 @@
 package main
 
-// $ "go get github.com/alexedwards/flow@latest"
-
 import (
-	"fmt"
 	"github.com/alexedwards/flow"
 	"log"
 	"net/http"
@@ -12,41 +9,18 @@ import (
 
 func main() {
 	mux := flow.New()
-	mux.Use(htmxMiddleware)
-	mux.HandleFunc("/", indexHandler, "GET")
-	mux.HandleFunc("/clicked", clickedHandler, "POST")
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		renderHTML(w, "index.html")
+	}, "GET")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func htmxMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		w.Header().Set("HX-Current-URL", r.URL.String())
-		next.ServeHTTP(w, r)
-	})
-}
-
-func indexHandler(w http.ResponseWriter, _ *http.Request) {
-	renderHTML(w, "index", nil)
-}
-
-func clickedHandler(w http.ResponseWriter, _ *http.Request) {
-	renderHTML(w, "clicked", nil)
-}
-
-func renderHTML(w http.ResponseWriter, tmpl string, data interface{}) {
-	tmplPath := fmt.Sprintf("%s.html", tmpl)
-	t, err := template.ParseFiles(tmplPath)
+func renderHTML(w http.ResponseWriter, fileName string) {
+	err := template.Must(template.ParseFiles(fileName)).ExecuteTemplate(w, fileName, nil)
 	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
