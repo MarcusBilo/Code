c005dd1719863515833e8752099211d27b79434c
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2023-11-28 20:04:20 +0000

    Update demo.go

--- GO/ErrorHandling/demo.go
+++ GO/ErrorHandling/demo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"runtime"
 )
 
@@ -16,7 +17,7 @@ func divide(a, b float64) (float64, error) {
 func Log(err error) {
 	if err != nil {
 		_, _, line, _ := runtime.Caller(1)
-		fmt.Println("Error:", err.Error(), "| Line:", line)
+		log.Fatal("- Error: ", err.Error(), " | Line:", line)
 	}
 }
 
@@ -27,9 +28,11 @@ func main() {
 	_, err2 := divide(10, 0)
 	if err2 != nil {
 		fmt.Println("Error:", err2)
+		return
 	}
 
 	if _, err3 := divide(10, 0); err3 != nil {
 		fmt.Println("Error:", err3)
+		return
 	}
 }
