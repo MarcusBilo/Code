9921903bf2ada64f5511288caaa2bc9c7f4ef3de
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2023-11-29 18:00:07 +0000

    Update demo.go

--- GO/Web/demo.go
+++ GO/Web/demo.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
-	indexHandler := func(w http.ResponseWriter, r *http.Request) {
-		renderHTML(w, "index.html")
-	}
-	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/data", data)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func index(w http.ResponseWriter, _ *http.Request) {
+	renderHTML(w, "index.html")
+}
+
+func data(w http.ResponseWriter, _ *http.Request) {
+	content := map[string]string{"Message": "This is data loaded via HTMX from Go!"}
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(content)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
