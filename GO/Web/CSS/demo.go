12ec1e7247e18852f73e0309b377f8ac623f6d85
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2023-11-30 23:34:22 +0100

    Add files via upload

---
+++ GO/Web/CSS/demo.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/data", data)
+	http.Handle("/styles.css", http.FileServer(http.Dir("./")))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func index(w http.ResponseWriter, _ *http.Request) {
+	renderHTML(w, "index.html")
+}
+
+func data(w http.ResponseWriter, _ *http.Request) {
+	content := map[string]string{"Message": "This is data loaded via HTMX from Go!"}
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(content)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func renderHTML(w http.ResponseWriter, fileName string) {
+	err := template.Must(template.ParseFiles(fileName)).ExecuteTemplate(w, fileName, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
