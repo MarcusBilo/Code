f4e1d5b9ee0974f13f3a230ab2eb38666b2f2d4d
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2023-12-21 22:01:02 +0000

    Update main.py

--- Python/MaliciousWeb/main.py
+++ Python/MaliciousWeb/main.py
@@ -1,74 +1,45 @@
-import numpy as np
 import pandas as pd
+from sklearn.utils import resample
 from os.path import exists
 
-path = "D:\Ablage\Dataset of Malicious and Benign Webpages"
 
-if not exists(path+"\Good_Compact_Webpages_Classification_test_data.csv"):
+def main():
+    path = "D:\Ablage\Dataset of Malicious and Benign Webpages"
     columns_to_keep = ["url", "content", "label"]
-    original_data_test = pd.read_csv(path+"\Webpages_Classification_test_data.csv")
-    selected_columns_test = original_data_test[columns_to_keep]
-    good_data_test = selected_columns_test[selected_columns_test["label"] == "good"]
-    good_data_test.to_csv(path+"\Good_Compact_Webpages_Classification_test_data.csv", index=False)
-    bad_data_test = selected_columns_test[selected_columns_test["label"] == "bad"]
-    bad_data_test.to_csv(path+"\Bad_Compact_Webpages_Classification_test_data.csv", index=False)
 
-if not exists(path + "\Good_Compact_Webpages_Classification_train_data.csv"):
-    columns_to_keep = ["url", "content", "label"]
-    original_data_train = pd.read_csv(path+"\Webpages_Classification_train_data.csv")
-    selected_columns_train = original_data_train[columns_to_keep]
-    good_data_train = selected_columns_train[selected_columns_train["label"] == "good"]
-    bad_data_train = selected_columns_train[selected_columns_train["label"] == "bad"]
-    good_data_train.to_csv(path+"\Good_Compact_Webpages_Classification_train_data.csv", index=False)
-    bad_data_train.to_csv(path+"\Bad_Compact_Webpages_Classification_train_data.csv", index=False)
+    if not exists(path + "\Good_Compact_Webpages_Classification_test_data.csv"):
+        original_data_test = pd.read_csv(path + "\Webpages_Classification_test_data.csv")
+        selected_columns_test = original_data_test[columns_to_keep]
+        good_data_test = selected_columns_test[selected_columns_test["label"] == "good"]
+        bad_data_test = selected_columns_test[selected_columns_test["label"] == "bad"]
+        good_data_test.to_csv(path + "\Good_Compact_Webpages_Classification_test_data.csv", index=False)
+        bad_data_test.to_csv(path + "\Bad_Compact_Webpages_Classification_test_data.csv", index=False)
 
-"""
-og_good_train = pd.read_csv(path+"\Good_Compact_Webpages_Classification_train_data.csv")
-og_good_test = pd.read_csv(path+"\Good_Compact_Webpages_Classification_test_data.csv")
-bad_train = pd.read_csv(path+"\Bad_Compact_Webpages_Classification_train_data.csv")
-bad_test = pd.read_csv(path+"\Bad_Compact_Webpages_Classification_test_data.csv")
+    if not exists(path + "\Good_Compact_Webpages_Classification_train_data.csv"):
+        original_data_train = pd.read_csv(path + "\Webpages_Classification_train_data.csv")
+        selected_columns_train = original_data_train[columns_to_keep]
+        good_data_train = selected_columns_train[selected_columns_train["label"] == "good"]
+        bad_data_train = selected_columns_train[selected_columns_train["label"] == "bad"]
+        good_data_train.to_csv(path + "\Good_Compact_Webpages_Classification_train_data.csv", index=False)
+        bad_data_train.to_csv(path + "\Bad_Compact_Webpages_Classification_train_data.csv", index=False)
 
-print("OG Good train\n", og_good_train.shape)
-print("OG Good test\n", og_good_test.shape)
-print("Bad train\n", bad_train.shape)
-print("Bad test\n", bad_test.shape)
-"""
+    good_train = pd.read_csv(path + "\Good_Compact_Webpages_Classification_train_data.csv")
+    good_test = pd.read_csv(path + "\Good_Compact_Webpages_Classification_test_data.csv")
+    bad_train = pd.read_csv(path + "\Bad_Compact_Webpages_Classification_train_data.csv")
+    bad_test = pd.read_csv(path + "\Bad_Compact_Webpages_Classification_test_data.csv")
 
-if not exists(path+"\Good_Compact_Webpages_Classification_test_data_split_0.csv"):
-    good_test_split = pd.read_csv(path+"\Good_Compact_Webpages_Classification_test_data.csv")
-    groups = good_test_split.groupby(np.arange(len(good_test_split.index)) // 22117)  # 353872 mod 16 = 0
-    for (frameno, frame) in groups:
-        frame.to_csv(path+f"\Good_Compact_Webpages_Classification_test_data_split_%s.csv" % frameno, index=False)
+    undersampled_good_test = resample(good_test, replace=False, n_samples=len(bad_test))
+    undersampled_good_train = resample(good_train, replace=False, n_samples=len(bad_train))
 
-if not exists(path+"\Good_Compact_Webpages_Classification_train_data_split_0.csv"):
-    good_train_split = pd.read_csv(path+"\Good_Compact_Webpages_Classification_train_data.csv")
-    good_train_split = good_train_split.iloc[:-11]  # Drop the last 11 rows
-    groups = good_train_split.groupby(np.arange(len(good_train_split.index)) // 73296)  # 1172747-11 mod 16 = 0
-    for (frameno, frame) in groups:
-        frame.to_csv(path+f"\Good_Compact_Webpages_Classification_train_data_split_%s.csv" % frameno, index=False)
+    train_data = pd.concat([undersampled_good_train, bad_train])
+    test_data = pd.concat([undersampled_good_test, bad_test])
 
-"""
-edit_good_train = pd.read_csv(path+"\Good_Compact_Webpages_Classification_train_data_split_0.csv")
-edit_good_test = pd.read_csv(path+"\Good_Compact_Webpages_Classification_test_data_split_0.csv")
-print("Edit Good train\n", edit_good_train.shape)
-print("Edit Good test\n", edit_good_test.shape)
-"""
+    print(train_data.head())
+    print(train_data.tail())
+    print("\n", "_" * 75, "\n")
+    print(test_data.head())
+    print(test_data.tail())
 
-# ----------------------------------------------------------------------------------------------- #
-# ----------------------------------------------------------------------------------------------- #
-# ----------------------------------------------------------------------------------------------- #
 
-train_data = pd.concat(map(pd.read_csv, [
-    path+"\Good_Compact_Webpages_Classification_train_data_split_0.csv",
-    path+"\Bad_Compact_Webpages_Classification_train_data.csv"
-    ]), ignore_index=True)
-
-test_data = pd.concat(map(pd.read_csv, [
-    path+"\Good_Compact_Webpages_Classification_test_data_split_0.csv",
-    path+"\Bad_Compact_Webpages_Classification_test_data.csv"
-    ]), ignore_index=True)
-
-print(train_data.head())
-print(train_data.shape)
-print(test_data.head())
-print(test_data.shape)
+if __name__ == "__main__":
+    main()
