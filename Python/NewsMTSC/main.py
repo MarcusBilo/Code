111cffc203d43c3595b99790cc5b1b25cb45c491
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2023-12-28 15:01:00 +0000

    Update main.py

--- Python/NewsMTSC/main.py
+++ Python/NewsMTSC/main.py
@@ -5,21 +5,39 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from tqdm import tqdm
 import json
+from sklearn.utils import resample
+import numpy as np
 
 nlp = spacy.load("en_core_web_md")
 
 
 def preprocess_text(data):
     processed_data = []
-    for text in tqdm(data, desc="Preprocessing"):
+    for text in data:
         doc = nlp(text)
         doc_vector = doc.vector
         processed_data.append(doc_vector)
     return processed_data
 
 
-def main():
+def undersample_classes(data, labels):
+    positive_indices = [i for i, label in enumerate(labels) if label == "positive"]
+    negative_indices = [i for i, label in enumerate(labels) if label == "negative"]
+    neutral_indices = [i for i, label in enumerate(labels) if label == "neutral"]
 
+    negative_resampled = resample(negative_indices, n_samples=len(positive_indices))
+    neutral_resampled = resample(neutral_indices, n_samples=len(positive_indices))
+
+    balanced_indices = positive_indices + list(negative_resampled) + list(neutral_resampled)
+    np.random.shuffle(balanced_indices)
+
+    balanced_data = [data[i] for i in balanced_indices]
+    balanced_labels = [labels[i] for i in balanced_indices]
+
+    return balanced_data, balanced_labels
+
+
+def load_data(x):
     train_data = []
     with open("train.jsonl", "r", encoding="utf-8") as train_file:
         for line in train_file:
@@ -37,34 +55,44 @@ def main():
     y_test = [item["targets"][0]["polarity"] for item in test_data]
 
     sentiment_map = {2.0: "negative", 4.0: "neutral", 6.0: "positive"}
+    train_labels_map = [sentiment_map[p] for p in y_train]
+    test_labels_map = [sentiment_map[p] for p in y_test]
 
-    train_labels = [sentiment_map[p] for p in y_train]
-    test_labels = [sentiment_map[p] for p in y_test]
+    if x == "undersampled":
+        train_data, train_labels = undersample_classes(train_data_normalized, train_labels_map)
+        test_data, test_labels = undersample_classes(test_data_normalized, test_labels_map)
+        return train_data, test_data, train_labels, test_labels
+    else:
+        return train_data_normalized, test_data_normalized, train_labels_map, test_labels_map
 
-    """
-    from collections import Counter
-    
-    occurrences_of_sentiment_train = Counter(train_labels)
-    occurrences_of_sentiment_test = Counter(test_labels)
-    print("Training set class counts:", occurrences_of_sentiment_train)
-    print("Test set class counts:", occurrences_of_sentiment_test)
-    
-    # -> ggf undersampling und oversampling nutzen
-    """
 
-    train_data = preprocess_text(train_data_normalized)
-    test_data = preprocess_text(test_data_normalized)
+def main():
+    train_accuracies = []
+    test_accuracies = []
 
-    clf = RandomForestClassifier(max_depth=5)
-    clf.fit(train_data, train_labels)
+    for _ in tqdm(range(10), desc="Processing", unit="iteration"):
+        train_data, test_data, train_labels, test_labels = load_data("undersampled")
 
-    train_predictions = clf.predict(train_data)
-    train_accuracy = accuracy_score(train_labels, train_predictions)
-    print(f"Training Accuracy: {train_accuracy:.4f}")
+        train_data = preprocess_text(train_data)
+        test_data = preprocess_text(test_data)
 
-    test_predictions = clf.predict(test_data)
-    test_accuracy = accuracy_score(test_labels, test_predictions)
-    print(f"Test Accuracy: {test_accuracy:.4f}")
+        clf = RandomForestClassifier(max_depth=5)
+        clf.fit(train_data, train_labels)
+
+        train_predictions = clf.predict(train_data)
+        train_accuracy = accuracy_score(train_labels, train_predictions)
+        train_accuracies.append(train_accuracy)
+
+        test_predictions = clf.predict(test_data)
+        test_accuracy = accuracy_score(test_labels, test_predictions)
+        test_accuracies.append(test_accuracy)
+
+    train_min, train_max = np.min(train_accuracies), np.max(train_accuracies)
+    test_min, test_max = np.min(test_accuracies), np.max(test_accuracies)
+    train_range, test_range = (train_max - train_min) / 2, (test_max - test_min) / 2
+
+    print(f"\nAverage Training Accuracy: {np.mean(train_accuracies):.3f} ± {train_range:.3f}")
+    print(f"Average Test Accuracy: {np.mean(test_accuracies):.3f} ± {test_range:.3f}")
 
 
 if __name__ == "__main__":
