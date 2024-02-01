f92c1b9bebc0b9c5a4b054d956674b07ba5dcbe6
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2024-02-01 13:37:24 +0100

    Update keras_tuner.py

--- Python/NewsMTSC/keras_tuner.py
+++ Python/NewsMTSC/keras_tuner.py
@@ -3,21 +3,19 @@ import os
 import silence_tensorflow.auto  # pip install silence-tensorflow
 import spacy
 import numpy as np
-from tabulate import tabulate
-from tqdm import tqdm
-from sklearn.metrics import accuracy_score
 from sklearn.utils import resample
-from sklearn.svm import SVC
-from keras.models import Sequential, Model
-from keras.layers import Conv1D, Dense, Flatten, LSTM, Bidirectional, Input, Masking, Dropout, GRU
 from keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
-from keras.losses import categorical_crossentropy
 from transformers import BertTokenizer, TFBertForSequenceClassification
-from keras.metrics import CategoricalAccuracy
 import tensorflow as tf
 import psutil
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer  # pip install vaderSentiment
+from keras_tuner.tuners import RandomSearch  # pip install keras-tuner
+from keras.models import Sequential
+from keras.layers import Conv1D, Dense, Dropout, Flatten, Masking
+from keras.losses import CategoricalCrossentropy
+from keras.optimizers import Adam
+from keras.metrics import CategoricalAccuracy
+from sklearn.model_selection import train_test_split
 
 
 tf.random.set_seed(2024)
@@ -124,227 +122,6 @@ def preprocess_labels(label_encoder, train_labels, test_labels, num_classes=3):
     test_labels_categorical = to_categorical(y=test_labels_encoded, num_classes=num_classes)
     return train_labels_categorical, test_labels_categorical
 
-"""
-
-class VADER:
-
-    def predict(self, x):
-        predictions = [self.analyze_sentiment(sentence) for sentence in x]
-        return predictions
-
-    @staticmethod
-    def analyze_sentiment(sentence):
-        analyzer = SentimentIntensityAnalyzer()
-        vs = analyzer.polarity_scores(sentence)
-        return vs['compound']
-
-    # https://github.com/cjhutto/vaderSentiment?tab=readme-ov-file#about-the-scoring
-    @staticmethod
-    def classify_sentiment(compound_score):
-        if compound_score > 0.05:
-            return 'positive'
-        elif compound_score < -0.05:
-            return 'negative'
-        else:
-            return 'neutral'
-
-
-def cnn_model():
-    model = Sequential()
-    model.add(Masking(mask_value=0))
-    model.add(Conv1D(filters=512, kernel_size=10, strides=5))
-    model.add(Conv1D(filters=512, kernel_size=10, strides=5))
-    model.add(Dropout(0.5))
-    model.add(Dense(units=512, activation="linear"))
-    # model.add(Dropout(0.5))
-    model.add(Flatten())
-    model.add(Dense(3, activation="softmax"))
-    model.compile(optimizer=adam, loss=categorical_crossentropy, metrics=CategoricalAccuracy())
-    model._name = "CNN"
-    return model
-
-
-def gru_model():
-    model = Sequential()
-    model.add(Masking(mask_value=0))
-    model.add(GRU(units=384, return_sequences=True))
-    model.add(GRU(units=384))
-    model.add(Dropout(0.5))
-    model.add(Dense(units=384, activation="linear"))
-    # model.add(Dropout(0.5))
-    model.add(Dense(3, activation="softmax"))
-    model.compile(optimizer=adam, loss=categorical_crossentropy, metrics=CategoricalAccuracy())
-    model._name = "GRU"
-    return model
-
-
-def bi_gru_model():
-    model = Sequential()
-    model.add(Masking(mask_value=0))
-    model.add(Bidirectional(GRU(units=192, return_sequences=True)))
-    model.add(Bidirectional(GRU(units=192)))
-    model.add(Dropout(0.5))
-    model.add(Dense(192, activation="linear"))
-    # model.add(Dropout(0.5))
-    model.add(Dense(3, activation="softmax"))
-    model.compile(optimizer=adam, loss=categorical_crossentropy, metrics=CategoricalAccuracy())
-    model._name = "Bi-GRU"
-    return model
-
-
-def lstm_model():
-    model = Sequential()
-    model.add(Masking(mask_value=0))
-    model.add(LSTM(units=384, return_sequences=True))
-    model.add(LSTM(units=384))
-    model.add(Dropout(0.5))
-    model.add(Dense(384, activation="linear"))
-    # model.add(Dropout(0.5))
-    model.add(Dense(3, activation="softmax"))
-    model.compile(optimizer=adam, loss=categorical_crossentropy, metrics=CategoricalAccuracy())
-    model._name = "LSTM"
-    return model
-
-
-def bi_lstm_model():
-    model = Sequential()
-    model.add(Masking(mask_value=0))
-    model.add(Bidirectional(LSTM(units=192, return_sequences=True)))
-    model.add(Bidirectional(LSTM(units=192)))
-    model.add(Dropout(0.5))
-    model.add(Dense(192, activation="linear"))
-    # model.add(Dropout(0.5))
-    model.add(Dense(3, activation="softmax"))
-    model.compile(optimizer=adam, loss=categorical_crossentropy, metrics=CategoricalAccuracy())
-    model._name = "Bi-LSTM"
-    return model
-
-
-def bert_model_1_128():
-    bert = TFBertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=3)
-    # only the last 1 layers are trainable
-    for layer in bert.layers:
-        layer.trainable = False
-    for layer in bert.layers[-1:]:
-        layer.trainable = True
-    input_ids = Input(shape=(100,), dtype="int32", name="input_ids")
-    attention_mask = Input(shape=(100,), dtype="int32", name="attention_mask")
-    masked_input = Masking(mask_value=0)(input_ids)
-    outputs = bert(masked_input, attention_mask=attention_mask)[0]
-    outputs = Dense(128, activation="linear")(outputs)
-    outputs = Dropout(0.5)(outputs)
-    outputs = Dense(3, activation='softmax')(outputs)
-    model = Model(inputs=[input_ids, attention_mask], outputs=outputs)
-    model.compile(optimizer=adam, loss=categorical_crossentropy, metrics=CategoricalAccuracy())
-    model._name = 'BERT_1_128'
-    return model
-
-
-def bert_model_2_256():
-    bert = TFBertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=3)
-    # only the last 2 layers are trainable
-    for layer in bert.layers:
-        layer.trainable = False
-    for layer in bert.layers[-2:]:
-        layer.trainable = True
-    input_ids = Input(shape=(100,), dtype="int32", name="input_ids")
-    attention_mask = Input(shape=(100,), dtype="int32", name="attention_mask")
-    masked_input = Masking(mask_value=0)(input_ids)
-    outputs = bert(masked_input, attention_mask=attention_mask)[0]
-    outputs = Dense(256, activation="linear")(outputs)
-    outputs = Dropout(0.5)(outputs)
-    outputs = Dense(3, activation='softmax')(outputs)
-    model = Model(inputs=[input_ids, attention_mask], outputs=outputs)
-    model.compile(optimizer=adam, loss=categorical_crossentropy, metrics=CategoricalAccuracy())
-    model._name = 'BERT_2_256'
-    return model
-
-
-def main():
-
-    classifiers = [
-        # VADER(),
-        # SVC(C=0.96, kernel="rbf", gamma="scale"),
-        # cnn_model(),
-        # gru_model(),
-        # bi_gru_model(),
-        # lstm_model(),
-        # bi_lstm_model(),
-        # bert_model_1_128(),
-        # bert_model_2_256(),
-    ]
-
-    results = []
-    train_accuracy, test_accuracy = 0.0, 0.0
-    label_encoder = LabelEncoder()
-
-    for _ in tqdm(range(1), desc=f"Preprocessing Data"):
-        train_data, test_data, train_labels, test_labels = load_data("undersampled")
-        train_data_sklearn, test_data_sklearn = preprocess_sklearn(train_data), preprocess_sklearn(test_data)
-        train_data_tf, test_data_tf = preprocess_tensorflow(train_data), preprocess_tensorflow(test_data)
-        train_labels_one_hot, test_labels_one_hot = preprocess_labels(label_encoder, train_labels, test_labels, num_classes=3)
-        train_data_bert, train_attention_mask = preprocess_bert(train_data)
-        test_data_bert, test_attention_mask = preprocess_bert(test_data)
-        train_data_bert, train_attention_mask = np.squeeze(train_data_bert, axis=1), np.squeeze(train_attention_mask, axis=1)
-        test_data_bert, test_attention_mask = np.squeeze(test_data_bert, axis=1), np.squeeze(test_attention_mask, axis=1)
-
-    for clf in classifiers:
-        iteration_losses = []
-        if isinstance(clf, VADER):
-            for _ in tqdm(range(1), desc=f"Processing {getattr(clf, 'name', clf.__class__.__name__)}", unit="Epoch"):
-                train_predictions = clf.predict(train_data)
-                train_sentiment_labels = [VADER.classify_sentiment(pred) for pred in train_predictions]
-                train_accuracy = accuracy_score(train_labels, train_sentiment_labels)
-                test_predictions = clf.predict(test_data)
-                test_sentiment_labels = [VADER.classify_sentiment(pred) for pred in test_predictions]
-                test_accuracy = accuracy_score(test_labels, test_sentiment_labels)
-                iteration_losses.append("---")
-        elif isinstance(clf, SVC):
-            for _ in tqdm(range(1), desc=f"Processing {getattr(clf, 'name', clf.__class__.__name__)}", unit="Epoch"):
-                clf.fit(train_data_sklearn, train_labels)
-                train_predictions = clf.predict(train_data_sklearn)
-                train_accuracy = accuracy_score(train_labels, train_predictions)
-                test_predictions = clf.predict(test_data_sklearn)
-                test_accuracy = accuracy_score(test_labels, test_predictions)
-                iteration_losses.append("---")
-        elif isinstance(clf, Sequential):
-            for _ in tqdm(range(10), desc=f"Processing {getattr(clf, 'name', clf.__class__.__name__)}", unit="Epoch"):
-                loss = clf.fit(train_data_tf, train_labels_one_hot, verbose=0, batch_size=71).history['loss'][0]
-                iteration_losses.append(round(loss, 4))
-                train_predictions = clf.predict(train_data_tf, verbose=0)
-                train_accuracy = accuracy_score(train_labels_one_hot.argmax(axis=1), np.argmax(train_predictions, axis=1))
-                test_predictions = clf.predict(test_data_tf, verbose=0)
-                test_accuracy = accuracy_score(test_labels_one_hot.argmax(axis=1), np.argmax(test_predictions, axis=1))
-        elif isinstance(clf, Model):
-            for _ in tqdm(range(10), desc=f"Processing {getattr(clf, 'name', clf.__class__.__name__)}", unit="Epoch"):
-                loss = clf.fit([train_data_bert, train_attention_mask], train_labels_one_hot, verbose=0, batch_size=10).history['loss'][0]
-                iteration_losses.append(round(loss, 4))
-                train_predictions = clf.predict([train_data_bert, train_attention_mask], verbose=0)
-                train_accuracy = accuracy_score(train_labels_one_hot.argmax(axis=1), np.argmax(train_predictions, axis=1))
-                test_predictions = clf.predict([test_data_bert, test_attention_mask], verbose=0)
-                test_accuracy = accuracy_score(test_labels_one_hot.argmax(axis=1), np.argmax(test_predictions, axis=1))
-        else:
-            raise Exception("no defined training for ", getattr(clf, 'name', clf.__class__.__name__))
-
-        results.append([
-            getattr(clf, 'name', clf.__class__.__name__),
-            round(train_accuracy, 4),
-            round(test_accuracy, 4),
-            iteration_losses
-        ])
-
-    headers = ["Classifier", "Train Acc", "Test Acc", "Loss"]
-    print("\n", tabulate(results, headers=headers, tablefmt="grid"))
-
-"""
-# pip install keras-tuner
-from keras_tuner.tuners import RandomSearch
-from keras.models import Sequential
-from keras.layers import Conv1D, Dense, Dropout, Flatten, Masking
-from keras.losses import CategoricalCrossentropy
-from keras.optimizers import Adam
-from keras.metrics import CategoricalAccuracy
-from sklearn.model_selection import train_test_split
 
 def build_cnn_model(hp):
     model = Sequential()
@@ -366,6 +143,7 @@ def build_cnn_model(hp):
     model.compile(optimizer=optimizer, loss=CategoricalCrossentropy(), metrics=[CategoricalAccuracy()])
     model._name = "CNN"
     return model
+
 
 def main():
     label_encoder = LabelEncoder()
