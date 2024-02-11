1eb31711b08e6e4c307d52d839583bead3076c4b
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2024-02-11 22:14:18 +0100

    Update demo.go

--- GO/Web/CSS/demo.go
+++ GO/Web/CSS/demo.go
@@ -10,6 +10,8 @@ import (
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/data", data)
+	http.HandleFunc("/favicon.ico", noFavicon)
+	http.HandleFunc("/htmx_v1.9.10.min.js", htmxHandler)
 	http.Handle("/styles.css", http.FileServer(http.Dir("./")))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
@@ -28,6 +30,15 @@ func data(w http.ResponseWriter, _ *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func noFavicon(w http.ResponseWriter, r *http.Request) {
+	// This handler does nothing, effectively returning no favicon
+}
+
+func htmxHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/javascript")
+	http.ServeFile(w, r, "htmx_v1.9.10.min.js") // Replace with the actual path
 }
 
 func renderHTML(w http.ResponseWriter, fileName string) {
