89e11348fd0b381d20551a2ac9f6d80270ae3a6c
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2024-02-19 14:02:22 +0100

    Update demo.go

--- GO/Web/CSS/demo.go
+++ GO/Web/CSS/demo.go
@@ -8,25 +8,34 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/index2", index2)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/index", http.StatusFound)
+	})
+	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		renderHTML(w, r, "index.html")
+	})
+	http.HandleFunc("/index2", func(w http.ResponseWriter, r *http.Request) {
+		renderHTML(w, r, "index2.html")
+	})
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		// https://www.flaticon.com/free-icon/quality_6294076
+		http.ServeFile(w, r, "quality.png")
+	})
+	http.HandleFunc("/3834171_80219.avif", func(w http.ResponseWriter, r *http.Request) {
+		// https://www.freepik.com/free-vector/geometric-triangle-pattern-illustration_3834171.htm
+		http.ServeFile(w, r, "3834171_80219.avif")
+	})
+
+	http.HandleFunc("/htmx_v1.9.10.min.js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		http.ServeFile(w, r, "htmx_v1.9.10.min.js")
+	})
 	http.HandleFunc("/data", data)
-	http.HandleFunc("/favicon.ico", favicon)
-	http.HandleFunc("/htmx_v1.9.10.min.js", htmxHandler)
-	http.HandleFunc("/3834171_80219.avif", imageHandler)
 	http.Handle("/styles.css", http.FileServer(http.Dir("./")))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func index(w http.ResponseWriter, _ *http.Request) {
-	renderHTML(w, "index.html")
-}
-
-func index2(w http.ResponseWriter, _ *http.Request) {
-	renderHTML(w, "index2.html")
 }
 
 func data(w http.ResponseWriter, _ *http.Request) {
@@ -38,25 +47,11 @@ func data(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
-func favicon(w http.ResponseWriter, r *http.Request) {
-	// https://www.flaticon.com/free-icon/quality_6294076
-	http.ServeFile(w, r, "quality.png")
-}
-
-func htmxHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/javascript")
-	http.ServeFile(w, r, "htmx_v1.9.10.min.js")
-}
-
-func imageHandler(w http.ResponseWriter, r *http.Request) {
-	// https://www.freepik.com/free-vector/geometric-triangle-pattern-illustration_3834171.htm
-	// https://convertio.co/de/jpg-avif/
-	// https://www.youcompress.com/avif/
-	http.ServeFile(w, r, "3834171_80219.avif")
-}
-
-func renderHTML(w http.ResponseWriter, fileName string) {
-	err := template.Must(template.ParseFiles(fileName)).ExecuteTemplate(w, fileName, nil)
+func renderHTML(w http.ResponseWriter, r *http.Request, fileName string) {
+	data := map[string]interface{}{
+		"RequestPath": r.URL.Path,
+	}
+	err := template.Must(template.ParseFiles(fileName)).ExecuteTemplate(w, fileName, data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
