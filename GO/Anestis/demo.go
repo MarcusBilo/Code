52bd4e6f5953962680465ae526e48eef0f82b37b
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2024-05-16 17:35:30 +0200

    Add files via upload

---
+++ GO/Anestis/demo.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bytes"
+	"encoding/csv"
+	ods "github.com/LIJUCHACKO/ods2csv"
+	"github.com/tealeg/xlsx/v3"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	executablePath, err := os.Executable()
+	if err != nil {
+		os.Exit(1)
+	}
+	directory := filepath.Dir(executablePath)
+	dir, err := os.Open(directory)
+	if err != nil {
+		os.Exit(1)
+	}
+	defer func(dir *os.File) {
+		err := dir.Close()
+		if err != nil {
+		}
+	}(dir)
+
+	files, err := dir.Readdir(-1)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".ods") {
+			fileContent, err := ods.ReadODSFile(directory + "/" + file.Name())
+			if err != nil {
+				continue
+			}
+			var buffer bytes.Buffer
+
+			for _, sheet := range fileContent.Sheets {
+				for _, row := range sheet.Rows {
+					for i, cell := range row.Cells {
+						buffer.WriteString(cell.Text)
+						if i < len(row.Cells)-1 {
+							buffer.WriteString(";")
+						}
+					}
+					buffer.WriteString("\n")
+				}
+			}
+
+			reader := csv.NewReader(&buffer)
+			reader.Comma = ';'
+			records, err := reader.ReadAll()
+			if err != nil {
+				return
+			}
+
+			uniqueEntries := make(map[string][]int)
+
+			for i, record := range records {
+				if i == 0 {
+					continue
+				}
+				if len(record) > 8 {
+					entry := record[8]
+					uniqueEntries[entry] = append(uniqueEntries[entry], i)
+				}
+			}
+
+			wb := xlsx.NewFile()
+			sheet, err := wb.AddSheet("Entries")
+			if err != nil {
+				return
+			}
+
+			row := sheet.AddRow()
+			row.AddCell().Value = "Artikelnummer"
+			row.AddCell().Value = "Artikelbezeichnung"
+			row.AddCell().Value = "Anzahl Bestellungen"
+			row.AddCell().Value = "Durchschnittliche Menge je Bestellung"
+			row.AddCell().Value = "Durchschnittlicher Bestellpreis"
+			row.AddCell().Value = "Gesamtkosten"
+
+			for entry, rows := range uniqueEntries {
+				row := sheet.AddRow()
+				row.AddCell().Value = entry
+				row.AddCell().Value = records[rows[0]][10]
+				row.AddCell().SetInt(len(rows))
+
+				sumAmount := 0
+				sumPrice := 0
+				for _, row := range rows {
+					amount, _ := strconv.Atoi(records[row][12])
+					sumAmount += amount
+					price, _ := strconv.Atoi(records[row][11])
+					sumPrice += price
+				}
+				row.AddCell().SetInt(sumAmount / len(rows))
+				row.AddCell().SetInt(sumPrice / len(rows))
+				row.AddCell().SetInt((sumAmount / len(rows)) * (sumPrice / len(rows)) * len(rows))
+			}
+
+			fileName := file.Name()
+			base := fileName[:len(fileName)-len(filepath.Ext(fileName))]
+			if err := wb.Save(base + ".xlsx"); err != nil {
+				return
+			}
+		}
+	}
+}
---
+++ GO/Anestis/go.mod
@@ -0,0 +1,22 @@
+module awesomeProject
+
+go 1.22rc2
+
+require (
+	github.com/LIJUCHACKO/ods2csv v0.0.0-20210510104632-b65f8a69c4ba
+	github.com/tealeg/xlsx/v3 v3.3.6
+)
+
+require (
+	github.com/LIJUCHACKO/XmlDB v0.0.0-20240507005148-e7e61047ffd2 // indirect
+	github.com/frankban/quicktest v1.14.6 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/peterbourgon/diskv/v3 v3.0.1 // indirect
+	github.com/rogpeppe/fastuuid v1.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
+	github.com/shabbyrobe/xmlwriter v0.0.0-20200208144257-9fca06d00ffa // indirect
+	golang.org/x/text v0.3.8 // indirect
+)
