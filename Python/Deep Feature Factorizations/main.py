051ab33180ea45205e8b43644309c37b0bfe1212
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2024-05-21 13:41:13 +0200

    Update main.py

--- Python/Deep Feature Factorizations/main.py
+++ Python/Deep Feature Factorizations/main.py
@@ -12,15 +12,29 @@ import matplotlib.pyplot as plt
 from torchvision.datasets import Imagenette
 import streamlit as st
 
+from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad, HiResCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+from torchvision.models import resnet18
+from PIL import Image
+import torch
+
 # https://github.com/jacobgil/pytorch-grad-cam
 # streamlit run C:/Users/Marcus/PycharmProjects/pythonProject1/main.py
 
-"""
 
 st.sidebar.header('Enter Numbers')
 num1 = st.sidebar.number_input('index', min_value=0, step=1, value=0, format='%d')
-num2 = st.sidebar.number_input('n comp', min_value=1, step=1, value=1, format='%d')
-num3 = st.sidebar.number_input('top k', min_value=1, step=1, value=1, format='%d')
+
+# Model selection in the sidebar
+model_choice = st.sidebar.selectbox("Choose a model", ["HiResCAM", "DFF"])
+
+if model_choice == "DFF":
+    num2 = st.sidebar.number_input('n comp', min_value=1, step=1, value=1, format='%d')
+    num3 = st.sidebar.number_input('top k', min_value=1, step=1, value=1, format='%d')
+else:
+    num2 = 1
+    num3 = 1
 
 image_index = int(num1)
 n_components = int(num2)
@@ -51,20 +65,6 @@ def create_labels(concept_scores, top_k=2):
     return concept_labels_topk
 
 
-def create_labels_v2(concept_scores, top_k=5):
-    imagenet_categories_url = \
-        "https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt"
-    labels = eval(requests.get(imagenet_categories_url).text)
-    concept_categories = np.argsort(concept_scores, axis=1)[:, ::-1][:, :top_k]
-    concept_labels_topk = []
-    for concept_index in range(concept_categories.shape[0]):
-        categories = concept_categories[concept_index, :]
-        concept_labels = [f"{labels[category].split(',')[0]}:{concept_scores[concept_index, category]:.2f}" for category
-                          in categories]
-        concept_labels_topk.append("\n".join(concept_labels))
-    return concept_labels_topk
-
-
 plt.rcParams['figure.dpi'] = 80
 
 
@@ -85,8 +85,6 @@ def visualize_image(model, image, n_components=1, top_k=1):
     result = np.hstack((img, visualization))
     return result
 
-
-# ----------------------------------------------------------------------------------------------------------------------
 
 warnings.filterwarnings('ignore')
 model = resnet18(pretrained=True)
@@ -112,122 +110,79 @@ label_map = {
 }
 
 image, label = train_data[image_index]
-_, _, input_tensor = get_image_from_file(image)
 
 model.eval()
 
-for param in model.parameters():
-    param.requires_grad = False
+if model_choice == "DFF":
 
-outputs = model(input_tensor)
+    _, _, input_tensor = get_image_from_file(image)
 
-predicted_classes = create_labels_v2(outputs.numpy(), top_k=3)
+    for param in model.parameters():
+        param.requires_grad = False
 
-f0 = plt.figure(0)
-ima = visualize_image(model, image, n_components=n_components, top_k=top_k)
-plt.imshow(ima)
-plt.show()
-
-if image is not None:
-    st.pyplot(f0, use_container_width=True)
-    col1, col2 = st.columns([2, 1])
-
-    with col1:
-        st.write("Top 3 predicted classes:")
-        for i, labels in enumerate(predicted_classes[0].split("\n"), 1):
-            st.write(f"{i}. {labels}")
-
-    with col2:
-        st.write("True class:\n")
-        st.write(label_map[label])
-
-"""
-
-# ----------------------------------------------------------------------------------------------------------------------
-
-# https://www.kaggle.com/code/antwerp/where-is-the-model-looking-for-gradcam-pytorch
-
-from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad, HiResCAM
-from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-from pytorch_grad_cam.utils.image import show_cam_on_image
-from torchvision.models import resnet18
-from PIL import Image
-import torch
-
-warnings.filterwarnings('ignore')
-
-
-def create_labels_v3(concept_scores, top_k=5):
-    imagenet_categories_url = \
-        "https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt"
-    labels = eval(requests.get(imagenet_categories_url).text)
-    concept_categories = np.argsort(concept_scores, axis=1)[:, ::-1][:, :top_k]
-    concept_labels_topk = []
-    for concept_index in range(concept_categories.shape[0]):
-        categories = concept_categories[concept_index, :]
-        concept_labels = [f"{labels[category].split(',')[0]}:{concept_scores[concept_index, category]:.2f}" for category
-                          in categories]
-        concept_labels_topk.append("\n".join(concept_labels))
-    return concept_labels_topk
-
-
-# Load model resnet18
-model = resnet18(pretrained=True)
-
-# Pick up layers for visualization
-target_layers = [model.layer4[-1]]
-
-path = "ILSVRC2012_val_00009346.JPEG"
-rgb_img = Image.open(path).convert('RGB')
-transform = transforms.Compose([
-    transforms.Resize((320, 320)),
-])
-rgb_img = transform(rgb_img)
-rgb_img = np.float32(rgb_img) / 255
-input_tensor = preprocess_image(rgb_img,
-                                mean=[0.485, 0.456, 0.406],
-                                std=[0.229, 0.224, 0.225])
-
-#
-
-# Forward pass the input through the model
-with torch.no_grad():
-    model.eval()
     outputs = model(input_tensor)
 
-# Apply softmax to get probabilities
-probabilities = torch.nn.functional.softmax(outputs, dim=1)
+    probabilities = torch.nn.functional.softmax(outputs, dim=1)
+    _, predicted_indices = torch.topk(probabilities, 3)
+    predicted_indices = predicted_indices.squeeze().tolist()
+    concept_labels = create_labels(probabilities.numpy(), 3)
 
-# Get the top predicted classes and their probabilities
-top_k = 3
-_, predicted_indices = torch.topk(probabilities, top_k)
-predicted_indices = predicted_indices.squeeze().tolist()
+    f0 = plt.figure(0)
+    ima = visualize_image(model, image, n_components=n_components, top_k=top_k)
+    plt.imshow(ima)
+    plt.show()
 
-# Get concept labels for the top predicted classes
-concept_labels = create_labels_v3(probabilities.numpy(), top_k)
+    if image is not None:
+        st.pyplot(f0, use_container_width=True)
+        col1, col2 = st.columns([2, 1])
 
-for i, labels in enumerate(concept_labels[0].split("\n"), 1):
-    print(f"{i}. {labels}")
+        with col1:
+            st.write("Top 3 predicted classes:")
+            for i, labels in enumerate(concept_labels[0].split("\n"), 1):
+                st.write(f"{i}. {labels}")
 
-#
+        with col2:
+            st.write("True class:\n")
+            st.write(label_map[label])
+else:
+    rgb_img = np.float32(image) / 255
+    input_tensor = preprocess_image(rgb_img,
+                                    mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
 
-# cam = GradCAM(model=model, target_layers=target_layers)
-cam = HiResCAM(model=model, target_layers=target_layers)
+    input_tensor.requires_grad_()
+    outputs = model(input_tensor)
 
-# You can also use it within a with statement, to make sure it is freed,
-# In case you need to re-create it inside an outer loop:
-# with GradCAM(model=model, target_layers=target_layers) as cam:
-#   ...
+    target_layers = [model.layer4[-1]]
+    probabilities = torch.nn.functional.softmax(outputs, dim=1)
+    _, predicted_indices = torch.topk(probabilities, 3)
+    predicted_indices = predicted_indices.squeeze().tolist()
+    probabilities_np = probabilities.detach().numpy()
+    concept_labels = create_labels(probabilities_np, 1)
 
-# We have to specify the target we want to generate
-# the Class Activation Maps for.
-# If targets is None, the highest scoring category
-# will be used.
+    for i, labels in enumerate(concept_labels[0].split("\n"), 1):
+        print(f"{i}. {labels}")
 
-grayscale_cam = cam(input_tensor=input_tensor)
-grayscale_cam = grayscale_cam[0, :]
-visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+    cam = HiResCAM(model=model, target_layers=target_layers)
+    grayscale_cam = cam(input_tensor=input_tensor)
+    grayscale_cam = grayscale_cam[0, :]
+    visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
 
-fig = Image.fromarray(visualization, 'RGB')
-plt.imshow(fig)
-plt.show()
+    f1 = plt.figure(1)
+    fig = Image.fromarray(visualization, 'RGB')
+    plt.imshow(fig)
+    plt.gcf().set_size_inches(4, 2)
+    plt.show()
+
+    if image is not None:
+        st.pyplot(f1, use_container_width=False)
+        col1, col2 = st.columns([2, 1])
+
+        with col1:
+            st.write("Top predicted class:")
+            for i, labels in enumerate(concept_labels[0].split("\n"), 1):
+                st.write(f"{i}. {labels}")
+
+        with col2:
+            st.write("True class:\n")
+            st.write(label_map[label])
