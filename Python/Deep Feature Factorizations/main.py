44f6cec124fdc45c0b73af688662c6e73d8fcb1f
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2024-06-02 14:19:13 +0200

    Update main.py

--- Python/Deep Feature Factorizations/main.py
+++ Python/Deep Feature Factorizations/main.py
@@ -1,5 +1,6 @@
 # pip install grad-cam
 # pip install streamlit
+# pip install "matplotlib<3.9"
 import warnings
 import numpy as np
 import requests
@@ -13,23 +14,18 @@ from pytorch_grad_cam import DeepFeatureFactorization, GradCAM, ScoreCAM, GradCA
 from pytorch_grad_cam.utils.image import show_cam_on_image
 from torchvision.models import resnet18
 from PIL import Image
+from matplotlib.gridspec import GridSpec
 
 # https://github.com/jacobgil/pytorch-grad-cam
 # streamlit run C:/Users/Marcus/PycharmProjects/pythonProject1/main.py
 
+st.set_page_config(layout="wide")
 
 st.sidebar.header('Enter Numbers')
 num1 = st.sidebar.number_input('index', min_value=0, step=1, value=0, format='%d')
 
-# Model selection in the sidebar
-model_choice = st.sidebar.selectbox("Choose a model", ["HiResCAM", "DFF"])
-
-if model_choice == "DFF":
-    num2 = st.sidebar.number_input('n comp', min_value=1, step=1, value=1, format='%d')
-    num3 = st.sidebar.number_input('top k', min_value=1, step=1, value=1, format='%d')
-else:
-    num2 = 1
-    num3 = 1
+num2 = st.sidebar.number_input('n comp', min_value=1, step=1, value=1, format='%d')
+num3 = st.sidebar.number_input('top k', min_value=1, step=1, value=1, format='%d')
 
 image_index = int(num1)
 n_components = int(num2)
@@ -60,7 +56,7 @@ def create_labels(concept_scores, top_k=2):
     return concept_labels_topk
 
 
-plt.rcParams['figure.dpi'] = 80
+plt.rcParams['figure.dpi'] = 90
 
 
 def visualize_image(model, image, n_components=1, top_k=1):
@@ -72,10 +68,12 @@ def visualize_image(model, image, n_components=1, top_k=1):
 
     concept_outputs = torch.softmax(torch.from_numpy(concept_outputs), axis=-1).numpy()
     concept_label_strings = create_labels(concept_outputs, top_k=top_k)
+    # Modify the list to add line breaks for two-word labels
+    modified_concept_labels = [label.replace(" ", "\n") if " " in label else label for label in concept_label_strings]
     visualization = show_factorization_on_image(rgb_img_float,
                                                 batch_explanations[0],
                                                 image_weight=0.5,
-                                                concept_labels=concept_label_strings)
+                                                concept_labels=modified_concept_labels)
 
     result = np.hstack((img, visualization))
     return result
@@ -104,77 +102,78 @@ label_map = {
     9: "parachute"
 }
 
-image, label = train_data[image_index]
+image, label = val_data[image_index]
 
 model.eval()
 
-if model_choice == "DFF":
+# HiResCAM setup
+rgb_img = np.float32(image) / 255
+input_tensor = preprocess_image(rgb_img,
+                                mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
 
-    _, _, input_tensor = get_image_from_file(image)
+input_tensor.requires_grad_()
+outputs_hirescam = model(input_tensor)
 
-    for param in model.parameters():
-        param.requires_grad = False
+target_layers = [model.layer4[-1]]
+probabilities_hirescam = torch.nn.functional.softmax(outputs_hirescam, dim=1)
+_, predicted_indices_hirescam = torch.topk(probabilities_hirescam, 3)
+predicted_indices_hirescam = predicted_indices_hirescam.squeeze().tolist()
+probabilities_np_hirescam = probabilities_hirescam.detach().numpy()
+concept_labels_hirescam = create_labels(probabilities_np_hirescam, 1)
 
-    outputs = model(input_tensor)
+cam = HiResCAM(model=model, target_layers=target_layers)
+grayscale_cam = cam(input_tensor=input_tensor)
+grayscale_cam = grayscale_cam[0, :]
+visualization_hirescam = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
 
-    probabilities = torch.nn.functional.softmax(outputs, dim=1)
-    _, predicted_indices = torch.topk(probabilities, 3)
-    predicted_indices = predicted_indices.squeeze().tolist()
-    concept_labels = create_labels(probabilities.numpy(), 3)
+# DFF setup
+_, _, input_tensor_dff = get_image_from_file(image)
 
-    f0 = plt.figure(0)
-    ima = visualize_image(model, image, n_components=n_components, top_k=top_k)
-    plt.imshow(ima)
-    plt.show()
+for param in model.parameters():
+    param.requires_grad = False
 
-    if image is not None:
-        st.pyplot(f0, use_container_width=True)
-        col1, col2 = st.columns([2, 1])
+outputs_dff = model(input_tensor_dff)
+probabilities_dff = torch.nn.functional.softmax(outputs_dff, dim=1)
+_, predicted_indices_dff = torch.topk(probabilities_dff, 3)
+predicted_indices_dff = predicted_indices_dff.squeeze().tolist()
+concept_labels_dff = create_labels(probabilities_dff.numpy(), 3)
 
-        with col1:
-            st.write("Top 3 predicted classes:")
-            for i, labels in enumerate(concept_labels[0].split("\n"), 1):
-                st.write(f"{i}. {labels}")
+ima_dff = visualize_image(model, image, n_components=n_components, top_k=top_k)
 
-        with col2:
-            st.write("True class:\n")
-            st.write(label_map[label])
-else:
-    rgb_img = np.float32(image) / 255
-    input_tensor = preprocess_image(rgb_img,
-                                    mean=[0.485, 0.456, 0.406],
-                                    std=[0.229, 0.224, 0.225])
+# Create figure with GridSpec
+fig = plt.figure(figsize=(12, 4))
+gs = GridSpec(1, 2, width_ratios=[1, 3])  # 1:2 ratio
 
-    input_tensor.requires_grad_()
-    outputs = model(input_tensor)
+# HiResCAM image
+ax1 = fig.add_subplot(gs[0])
+ax1.imshow(visualization_hirescam)
+ax1.axis('off')
+ax1.set_title("HiResCAM")
 
-    target_layers = [model.layer4[-1]]
-    probabilities = torch.nn.functional.softmax(outputs, dim=1)
-    _, predicted_indices = torch.topk(probabilities, 3)
-    predicted_indices = predicted_indices.squeeze().tolist()
-    probabilities_np = probabilities.detach().numpy()
-    concept_labels = create_labels(probabilities_np, 1)
+# DFF image
+ax2 = fig.add_subplot(gs[1])
+ax2.imshow(ima_dff)
+ax2.axis('off')
+ax2.set_title("DFF")
 
-    cam = HiResCAM(model=model, target_layers=target_layers)
-    grayscale_cam = cam(input_tensor=input_tensor)
-    grayscale_cam = grayscale_cam[0, :]
-    visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+# Adjust the spacing between subplots and margins around the plot
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.show()
 
-    f1 = plt.figure(1)
-    fig = Image.fromarray(visualization, 'RGB')
-    plt.imshow(fig)
-    plt.gcf().set_size_inches(4, 2)
-    plt.show()
 
-    if image is not None:
-        st.pyplot(f1, use_container_width=False)
-        col1, col2 = st.columns([2, 1])
+# Streamlit display
+if image is not None:
+    st.pyplot(fig, use_container_width=True)
 
-        with col1:
-            st.write("Top predicted class:")
-            for i, labels in enumerate(concept_labels[0].split("\n"), 1):
-                st.write(f"{i}. {labels}")
+    # Create empty columns on either side of the main columns to center them
+    col_left, col1, col2, col_right = st.columns([2, 2, 2, 1])
 
-        with col2:
-            st.write("True class:\n")
-            st.write(label_map[label])
+    with col1:
+        st.write("Top predicted classes:")
+        for i, labels in enumerate(concept_labels_hirescam[0].split("\n"), 1):
+            st.write(f"{i}. {labels}")
+
+    with col2:
+        st.write("True class:\n")
+        st.write(label_map[label])
