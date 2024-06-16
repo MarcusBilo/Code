5311476fe8678ddd432b6237785570de58e73519
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2024-06-16 21:50:11 +0200

    Update demo.go

--- GO/ods2csv/demo.go
+++ GO/ods2csv/demo.go
@@ -6,7 +6,8 @@ import (
 )
 
 func ReplaceHTMLSpecialEntities1(input string) string {
-	output := strings.Replace(input, "&amp;", "&", -1)
+	var output string
+	output = strings.Replace(input, "&amp;", "&", -1)
 	output = strings.Replace(output, "&lt;", "<", -1)
 	output = strings.Replace(output, "&gt;", ">", -1)
 	output = strings.Replace(output, "&quot;", "\"", -1)
@@ -21,23 +22,52 @@ func ReplaceHTMLSpecialEntities1(input string) string {
 }
 
 func ReplaceHTMLSpecialEntities2(input string) string {
-	output := input
-	replaceMap := map[string]string{
-		"&amp;":   "&",
-		"&lt;":    "<",
-		"&gt;":    ">",
-		"&quot;":  "\"",
-		"&lsquo;": "‘",
-		"&rsquo;": "’",
-		"&tilde;": "~",
-		"&ndash;": "–",
-		"&mdash;": "—",
-		"&apos;":  "'",
+	var b strings.Builder
+	b.Grow(len(input))
+	i := 0
+	for i < len(input) {
+		if input[i] == '&' {
+			switch {
+			case strings.HasPrefix(input[i:], "&amp;"):
+				b.WriteString("&")
+				i += len("&amp;")
+			case strings.HasPrefix(input[i:], "&lt;"):
+				b.WriteString("<")
+				i += len("&lt;")
+			case strings.HasPrefix(input[i:], "&gt;"):
+				b.WriteString(">")
+				i += len("&gt;")
+			case strings.HasPrefix(input[i:], "&quot;"):
+				b.WriteString("\"")
+				i += len("&quot;")
+			case strings.HasPrefix(input[i:], "&lsquo;"):
+				b.WriteString("‘")
+				i += len("&lsquo;")
+			case strings.HasPrefix(input[i:], "&rsquo;"):
+				b.WriteString("’")
+				i += len("&rsquo;")
+			case strings.HasPrefix(input[i:], "&tilde;"):
+				b.WriteString("~")
+				i += len("&tilde;")
+			case strings.HasPrefix(input[i:], "&ndash;"):
+				b.WriteString("–")
+				i += len("&ndash;")
+			case strings.HasPrefix(input[i:], "&mdash;"):
+				b.WriteString("—")
+				i += len("&mdash;")
+			case strings.HasPrefix(input[i:], "&apos;"):
+				b.WriteString("'")
+				i += len("&apos;")
+			default:
+				b.WriteByte(input[i])
+				i++
+			}
+		} else {
+			b.WriteByte(input[i])
+			i++
+		}
 	}
-	for entity, char := range replaceMap {
-		output = strings.Replace(output, entity, char, -1)
-	}
-	return output
+	return b.String()
 }
 
 func main() {
