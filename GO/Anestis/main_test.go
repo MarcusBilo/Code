ef617ca8db2158bab001378e84d5e75da24d8de3
MarcusBilo <85160148+MarcusBilo@users.noreply.github.com>
2024-06-23 03:06:22 +0200

    Update main_test.go

--- GO/Anestis/main_test.go
+++ GO/Anestis/main_test.go
@@ -8,18 +8,8 @@ import (
 
 // go test -bench . -benchmem
 
-// go test -bench . -benchmem -memprofile mem.prof
-//go tool pprof mem.prof
-
-// go test -bench . -benchmem -cpuprofile cpu.prof
-// go tool pprof cpu.prof
-
 func BenchmarkMain(b *testing.B) {
-	gcpercent := debug.SetGCPercent(-1)
-	memlimit := debug.SetMemoryLimit(math.MaxInt64)
 	for i := 0; i < b.N; i++ {
 		main()
 	}
-	debug.SetGCPercent(gcpercent)
-	debug.SetMemoryLimit(memlimit)
 }
